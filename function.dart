f2086b6985b68c11047e2e91d17995e431640fb2
Tushar-programador. <Tusharkalra307@gmail.com.>
2024-09-24 10:16:10 +0530

    basic concepts completed

---
+++ function.dart
@@ -0,0 +1,9 @@
+int sumOfAandB(int num1, int num2) {
+  return num1 + num2;
+}
+
+void main() {
+  int a = 7;
+  int b = 8;
+  print(sumOfAandB(a, b));
+}
--- symbol.dart
+++ symbol.dart
@@ -2,5 +2,5 @@ void main() {
   Symbol v = new Symbol("helo");
   print(v.runtimeType);
   print(v);
-  v(#cow) = "bufflo";
+  // v(#cow) = "bufflo";
 }
